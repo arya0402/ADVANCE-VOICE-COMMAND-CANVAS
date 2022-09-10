9b56c24ccbfd0c2e9c64c11e5ea8a1aa62a437c0
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-09-10 15:50:02 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ function draw() {
     speak();
     draw_apple = "";
   }
-  for (var i = 1; i == to_number; i++) {
+  for (var i = 1; i <= to_number; i++) {
     x = Math.floor(Math.random() * 700);
     y = Math.floor(Math.random() * 400);
     image(apple, x, y, 50, 50);
